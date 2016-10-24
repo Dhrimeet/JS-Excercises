842b391e1d6931bd5f9ad6538d9cb46f40973e27
Dhrimeet <dhrimeet@gmail.com>
2016-10-24 19:35:33 +0530

    JS Excercise

---
+++ DOMAccessQuiz/app.js
@@ -0,0 +1,90 @@
+var marg = 0;
+
+function identifyById() {
+
+}
+
+function identifyByClass() {
+
+}
+
+function byTag() {
+
+}
+
+function changeBackground() {
+
+}
+
+function increaseFont() {
+
+}
+
+function changeFontColor() {
+
+}
+
+function revertColor() {
+
+}
+
+function hide() {
+
+}
+
+function changeBackColor(color, className) {
+
+}
+
+function numbersOnly(e) {
+
+
+}
+
+function addAdjacent() {
+
+}
+
+function removePara() {
+
+}
+
+function myMove() {
+
+}
+
+function myFunction() {
+
+}
+
+function changeOn() {
+}
+
+function preferedBrowser() {
+}
+
+function color(elem) {
+}
+
+function inputxt() {
+}
+
+function confirmInput() {
+
+}
+
+function message() {
+  
+}
+
+function keydown(n) {
+}
+
+function keypress(n) {
+}
+
+function keyup() {
+}
+
+function writeMessage() {
+}
